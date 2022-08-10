eded165f2fc8e9e59333563386b94fafc07fdc4d
Michael Prattinger <mprattinger@outlook.com>
2022-08-10 15:38:20 +0200

    working on it

--- build/Build.cs
+++ build/Build.cs
@@ -14,7 +14,15 @@ using Nuke.Common.Utilities.Collections;
 using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
 using static Nuke.Common.IO.PathConstruction;
+using Nuke.Common.CI.GitHubActions;
 
+[GitHubActions("ci",
+    GitHubActionsImage.UbuntuLatest,
+    AutoGenerate = true,
+    OnPushBranches = new[] { "main" },
+    OnPullRequestBranches = new[] { "main" },
+    InvokedTargets = new[] { nameof(PushNuget) },
+    ImportSecrets = new[] { "NUGET_API_KEY" })]
 class Build : NukeBuild
 {
     /// Support plugins are available for:
@@ -23,10 +31,13 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+    public static int Main () => Execute<Build>(x => x.PushNuget);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter] string NugetApiUrl = "https://api.nuget.org/v3/index.json"; //default
+    [Parameter] string NugetApiKey;
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
@@ -79,9 +90,66 @@ class Build : NukeBuild
     Target BuildDocker => _ => _
         .DependsOn(Test)
         .Executes(() => {
-            DockerTasks.DockerBuild(d => {
-                d.SetFile(RootDirectory / "Dockerfile");
-                return d;
-            });
+            DockerTasks.DockerBuild(d => 
+                d.SetPath(RootDirectory)
+                .SetFile(SourceDirectory / "Becom.ISY.Weather" / "Dockerfile")
+                .SetTag(new[] { "isyweather", "mprattinger/isywather" })
+            );
+        });
+
+    Target CompileNuget => _ => _
+        .DependsOn(Test)
+    .Executes(() => {
+        DotNetTasks.DotNetBuild(s =>
+            s.SetProjectFile(SourceDirectory / "Becom.ISY.Weather.Contracts" / "Becom.ISY.Weather.Contracts.csproj")
+            .SetConfiguration(Configuration)
+                    .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                    .SetFileVersion(GitVersion.AssemblySemFileVer)
+                    .SetInformationalVersion(GitVersion.InformationalVersion)
+                    .EnableNoRestore()
+            );
+    });
+
+    Target PublishNuget => _ => _
+        .DependsOn(CompileNuget)
+    .Executes(() => {
+        DotNetTasks.DotNetPublish(s =>
+            s.SetProject(SourceDirectory / "Becom.ISY.Weather.Contracts" / "Becom.ISY.Weather.Contracts.csproj")
+            .SetOutput(ContractOutputDirectory)
+            );
+    });
+    Target PackNuget => _ => _
+        .DependsOn(PublishNuget)
+        .Executes(() => {
+            DotNetTasks.DotNetPack(s => s
+                .SetProject(SourceDirectory / "Becom.ISY.Weather.Contracts" / "Becom.ISY.Weather.Contracts.csproj")
+                .SetConfiguration(Configuration)
+                .EnableNoBuild()
+                .EnableNoRestore()
+                .SetVersion(GitVersion.NuGetVersionV2)
+                .SetIncludeSymbols(true)
+                .SetSymbolPackageFormat(DotNetSymbolPackageFormat.snupkg)
+                .SetOutputDirectory(ArtifactsDirectory)
+            );
+        });
+
+    Target PushNuget => _ => _
+        .DependsOn(PackNuget)
+        .Requires(() => NugetApiUrl)
+        .Requires(() => NugetApiKey)
+        .Requires(() => Configuration.Equals(Configuration.Release))
+        .Executes(() =>
+        {
+            GlobFiles(ArtifactsDirectory, "*.nupkg")
+               .NotEmpty()
+               .Where(x => !x.EndsWith("symbols.nupkg"))
+               .ForEach(x =>
+               {
+                   DotNetTasks.DotNetNuGetPush(s => s
+                       .SetTargetPath(x)
+                       .SetSource(NugetApiUrl)
+                       .SetApiKey(NugetApiKey)
+                   );
+               });
         });
 }
--- src/Becom.ISY.Weather/Program.cs
+++ src/Becom.ISY.Weather/Program.cs
@@ -1,12 +1,27 @@
+using Becom.ISY.Weather.Extensions;
+using Becom.ISY.Weather.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddWeather(builder.Configuration);
 
-//builder.Services.AddGrpc();
+// Add services to the container.
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.MapGet("/", async (IWeatherService weatherService) =>
+{
+    var res = await weatherService.LoadWeather();
+    if(res.HasException)
+    {
+        return Results.BadRequest(res.Exception);
+    }
+
+    return Results.Ok(res.ReturnedValue);
+});
+
+app.MapGet("/healthz", () => "OK");
 
 app.Run();
